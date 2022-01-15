de2665940c4c41841d056bd11e62b1d8bc369cfc
Patrick J Cherry <patch@headtube.dominoid.net>
2022-01-15 15:43:00 +0000

    Refactor
    Move the text/icon setting code into a reusable method

--- lib/info_display.rb
+++ lib/info_display.rb
@@ -12,28 +12,55 @@ class InfoDisplay
     @easy_tide = EasyTide.new(station_id: ENV['EASYTIDE_PORT_ID'])
     @template = template
     @svg = File.open(template) { |f| Nokogiri::XML(f) }
+    @now = Time.now
   end
 
   def root_path
     @root_path ||= File.dirname(@template)
   end
 
+  def icon_path_for(icon:)
+    "#{root_path}/weather-icons/wi-#{MetOfficeDatapoint::WEATHER_ICON_MAP[icon]}.svg"
+  end
+
   def update
-    svg.at_css('text#day tspan').content = datapoint.three_hourly.issued_at.strftime('%A')
-    svg.at_css('text#date tspan').content = datapoint.three_hourly.issued_at.strftime('%d %B')
-    svg.at_css('text#issued_at tspan').content = "Forecast issued at #{datapoint.three_hourly.issued_at.strftime('%l:%M %P')}"
+    set_text(id: 'day', content: Time.now.strftime('%A'))
+    set_text(id: 'date', content: Time.now.strftime('%d %B'))
+    update_forecast
+    update_tide
+  end
 
-    (0..3).each do |i|
-      svg.at_css("text#time_#{i} tspan").content = datapoint.three_hourly.future_forecasts[i]['time'].strftime('%l%P')
-      svg.at_css("text#temp_#{i} tspan").content = "#{datapoint.three_hourly.future_forecasts[i]['T']}°"
-      svg.at_css("text#pp_#{i} tspan").content = "#{datapoint.three_hourly.future_forecasts[i]['Pp']}%"
-      svg.at_css("text#wind_#{i} tspan").content = datapoint.three_hourly.future_forecasts[i]['S']
-      svg.at_css("image#weather_icon_#{i}")['xlink:href'] =
-        "#{root_path}/weather-icons/wi-#{MetOfficeDatapoint::WEATHER_ICON_MAP[datapoint.three_hourly.future_forecasts[i]['W']]}.svg"
+  def update_forecast
+    datapoint.three_hourly.future_forecasts.each_with_index do |forecast, i|
+      set_text(id: "time_#{i}", content: forecast['time'].strftime('%l%P'))
+      set_text(id: "temp_#{i}", content: "#{forecast['T']}°")
+      set_text(id: "pp_#{i}", content: "#{forecast['Pp']}%")
+      set_text(id: "wind_#{i}", content: forecast['S'])
+      set_weather_icon(id: "weather_icon_#{i}", icon: forecast['W'])
     end
+    set_text(id: 'issued_at',
+             content: "Forecast issued at #{datapoint.three_hourly.issued_at}")
+  end
 
-    svg.at_css('text#tide_time tspan').content = easy_tide.next_high_tide.time.strftime('%H:%M')
-    svg.at_css('text#tide_height tspan').content = format('%.2f', easy_tide.next_high_tide.height)
+  def update_tide
+    set_text(id: 'tide_time', content: easy_tide.next_high_tide.time.strftime('%H:%M'))
+    set_text(id: 'tide_height', content: format('%.2fm', easy_tide.next_high_tide.height))
+  end
+
+  def set_text(id: nil, content: nil, selector: nil)
+    selector ||= "text##{id} tspan"
+    element = svg.css(selector).first
+    return unless element
+
+    element.content = content
+  end
+
+  def set_weather_icon(id: nil, icon: nil, path: nil, selector: nil)
+    selector ||= "image##{id}"
+    element = svg.css(selector).first
+    return unless element
+
+    element['xlink:href'] = path || icon_path_for(icon: icon)
   end
 
   def to_s
