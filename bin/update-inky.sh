6d9e5656a720d9658c6a37e2236ff3215f44555e
Patrick J Cherry <patch@headtube.dominoid.net>
2022-01-15 12:15:12 +0000

    Added noddy script

---
+++ bin/update-inky.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+BASENAME='info-display'
+
+cd $(dirname $0)/..
+
+bundle exec bin/generate-info-display inky-impression.svg > $BASENAME.svg
+rsvg-convert -b white -o $BASENAME.png $BASENAME.svg
+
+~/Pimoroni/inky/examples/7color/image.py $BASENAME.png
